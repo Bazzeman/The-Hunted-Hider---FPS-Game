f942405b0b6c433ba0e2494931c70c300b1328c0
Bop191 <110249979+Bop191@users.noreply.github.com>
2023-11-24 12:31:27 +0100

    Added Unity files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float walkSpeed = 10F;
+    public float sprintSpeed = 15F;
+    public float jumpSpeed = 0.5F;
+    public float gravity = -0.15F;
+
+    private float gravityB;
+    private float speedX;
+    private float speedZ;
+    private float speed;
+    private Vector3 move; // Can be improved
+
+    public CharacterController controller;
+
+    void Start()
+    {
+        speed = walkSpeed;
+        gravityB = gravity;
+    }
+
+    void Update()
+    {
+        speedX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        speedZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        move = transform.right * speedX
+            + transform.up * gravity
+            + transform.forward * speedZ;
+
+        controller.Move(move);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = sprintSpeed;
+        }
+        else
+        {
+            speed = walkSpeed;
+        }
+
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            gravity = gravityB;
+            gravity *= -jumpSpeed;
+        }
+
+        if (gravity > gravityB)
+        {
+            gravity -= jumpSpeed * Time.deltaTime;
+        }
+    }
+}
